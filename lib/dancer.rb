79568a104fe97e0b177e612196661fcb7d237cf7
Michael Waldo <mbwcogic@yahoo.com>
2020-01-29 03:24:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
-   extend MetaDancing
-  include Dance
+   #extend MetaDancing
+  #include Dance
+  
  
   attr_accessor :name
  
